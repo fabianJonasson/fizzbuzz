e3d341776fa3a8575f44cdb48bf6d1eea62c5929
fabianJonasson <fabian.jonasson@kuberacad.se>
2023-09-12 18:47:37 +0200

    implemented tasks 1,2 and 3

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-for(let i = 1; i <=100; i++){
+function fizzbuzz(start, end) {
+   for(let i = start; i <=end; i++){
     if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
     else if (i % 3 === 0) console.log("Fizz");
     else if (i % 5 === 0) console.log("Buzz");
+    else if (i % 7 === 0) console.log("Boom");
     else console.log(i);
+    } 
 }
+fizzbuzz(1,200);
